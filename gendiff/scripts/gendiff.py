599e66ab05cee60ac3f0af98756cf6d4aa80fdcb
Roman <roma.mazin.47@gmail.com>
2025-11-07 23:13:54 +0300

    Add gendiff, scripts, pyproject

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,22 @@
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Compares two configuration files and shows a difference.'
+    )
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+    parser.add_argument('-f', '--format',
+                        help='set format of output',
+                        default='stylish')
+
+    args = parser.parse_args()
+
+    # Временная реализация для теста
+    print(f"Comparing {args.first_file} and {args.second_file}")
+    print(f"Output format: {args.format}")
+
+
+if __name__ == '__main__':
+    main()
