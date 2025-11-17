98f07f20206de2bc2ea58b8c11ff098b29a21d37
Roman <roma.mazin.47@gmail.com>
2025-11-17 20:45:42 +0300

    Added yaml and pytest support for them

--- gendiff/scripts/parser.py
+++ gendiff/scripts/parser.py
@@ -2,6 +2,9 @@ import json
 import os
 
 
+import yaml
+
+
 def get_file_format(file_path):
     _, extension = os.path.splitext(file_path)
     return extension[1:]
@@ -15,6 +18,8 @@ def read_file(file_path):
 def parse_data(data, format):
     if format == 'json':
         return json.loads(data)
+    if format == 'yaml' or format == 'yml':
+        return yaml.safe_load(data)
     raise ValueError(f'Unsupported file format: {format}')
 
 
--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -6,8 +6,10 @@ from gendiff.scripts.parser import read_file
 @pytest.mark.parametrize('file_path1, file_path2, expected_result', [
     ('tests/test_data/file1.json',
      'tests/test_data/file2.json',
-     'tests/test_data/expected_result_stylish.txt.txt')])
-
+     'tests/test_data/expected_result_json.txt'),
+    ('tests/test_data/file1.yaml',
+     'tests/test_data/file2.yaml',
+     'tests/test_data/expected_result_yaml.txt')])
 def test_generate_diff(file_path1, file_path2, expected_result):
     diff = generate_diff(file_path1, file_path2)
     expected = read_file(expected_result).strip()
