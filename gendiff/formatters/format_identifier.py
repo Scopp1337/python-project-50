90d76dc349f3530de9fe8d435ab59896724e5415
Roman <roma.mazin.47@gmail.com>
2025-11-18 19:38:35 +0300

    Add format plain and pytest

--- gendiff/formatters/format_identifier.py
+++ gendiff/formatters/format_identifier.py
@@ -1,4 +1,5 @@
 from gendiff.formatters.json_formatter import format_diff_json
+from gendiff.formatters.plain import format_diff_plain
 from gendiff.formatters.stylish import format_diff_stylish
 
 
@@ -7,5 +8,7 @@ def format_identifier(diff, formatter):
         return format_diff_stylish(diff)
     if formatter == 'json':
         return format_diff_json(diff)
+    if formatter == 'plain':
+        return format_diff_plain(diff)
     else:
         raise ValueError(f"Unsupported formatter: {formatter}")
--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -16,6 +16,19 @@ def test_generate_diff(file_path1, file_path2, expected_result):
     assert diff.strip() == expected
 
 
+@pytest.mark.parametrize('file_path1, file_path2, expected_result', [
+    ('tests/test_data/file1.json',
+     'tests/test_data/file2.json',
+     'tests/test_data/expected_result_plain.txt'),
+    ('tests/test_data/file1.yaml',
+     'tests/test_data/file2.yaml',
+     'tests/test_data/expected_result_plain.txt')])
+def test_generate_diff(file_path1, file_path2, expected_result):
+    diff = generate_diff(file_path1, file_path2, formatter='plain')
+    expected = read_file(expected_result).strip()
+    assert diff.strip() == expected
+
+
 
 
 
